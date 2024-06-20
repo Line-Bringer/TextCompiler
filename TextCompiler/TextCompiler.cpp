c4c0d82516d92edd1a0411acaa2e9bc28882cecb
Line-Bringer <kpsharma31st@gmail.com>
2024-06-19 19:28:12 -0700

    Update TextCompiler.cpp

--- TextCompiler/TextCompiler.cpp
+++ TextCompiler/TextCompiler.cpp
@@ -14,10 +14,12 @@ int main(int argc, char* argv[]){
 
     cout << "Enter the first file: " << endl;
     cin >> filename;
+    filename = filename + ".txt";
     ifstream file1(filename);
 
     cout << "\nEnter the second file: " << endl;
     cin >> filename;
+    filename = filename + ".txt";
     ifstream file2(filename);
 
     //read and store first file lines
@@ -42,6 +44,7 @@ int main(int argc, char* argv[]){
     //output file for compiled
     cout << "Enter desired output file name: " << endl;
     cin >> filename;
+    filename = filename + ".txt";
     cout << "Chosen output file name is: " << filename << endl;
 
     ofstream outputFile(filename);
